46a2e5fefa67f08334b82a439a3fedbf81606cee
olliswe <oliveriyer@gmail.com>
2020-02-21 16:27:31 +0000

    added auth

--- osusu/settings.py
+++ osusu/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "osusu_system",
     "corsheaders",
     "django_filters",
+    "rest_auth",
 ]
 
 MIDDLEWARE = [
--- osusu_system/urls.py
+++ osusu_system/urls.py
@@ -18,4 +18,5 @@ router.register(r"parts", views.PartViewSet)
 urlpatterns = [
     url(r"^", include(router.urls)),
     url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    url(r"^rest-auth/", include("rest_auth.urls")),
 ]
